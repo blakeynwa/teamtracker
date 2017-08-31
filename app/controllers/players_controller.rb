28c6fd80dc64e8344821f63bbc8a70ddb8718f81
Carter McGee <carterm912@gmail.com>
2017-08-31 10:40:06 -0700

    create all routes in player controller, add twilio gem to gemfile

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,26 +4,36 @@ class PlayersController < ApplicationController
   end
 
   def new
-
+    @player = Player.new
   end
 
   def create
     @players = Player.new(player_params)
 
-    @player.save
-    redirect_to @player
+    if @player.save
+      redirect_to @player, notice: "#{@player.name} was successfully added."
+    else
+      render :new, status: 422
+    end
   end
 
   def edit
-
+    @player = Player.find(params[:id])
   end
 
   def update
-
+      @player = Player.find(params[:id])
+    if @player.update(player_params)
+      redirect_to @player
+    else
+      render 'edit'
+    end
   end
 
   def delete
-
+    player = Player.find(params[:id])
+    player.destroy
+    redirect_to players_url, notice: 'Player was successfully removed.'
   end
 
   def show
