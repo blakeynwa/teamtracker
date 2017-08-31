eaa1a00042843f5915caa0cec8d281a46e08143c
Blake Meyer <blake.ynwa@gmail.com>
2017-08-31 10:24:49 -0700

    Complete buildout of teams - all views, controllers created and functioning. Need to write rspec tests

--- db/migrate/20170831003955_create_teams.rb
+++ db/migrate/20170831003955_create_teams.rb
@@ -1,6 +1,7 @@
 class CreateTeams < ActiveRecord::Migration[5.1]
   def change
     create_table :teams do |t|
+      t.string :name
       t.string :sport
       t.integer :number_required_to_play
 
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  team = Team.new(sport: "softball", number_required_to_play: 5)
+  describe "team is instantiated with attributes" do 
+    it "has a specific sport" do 
+      expect(team.sport).to eq("softball")
+    end
+
+    it "has a number of players required to play" do 
+      expect(team.number_required_to_play).to eq(5)
+    end
+  end
 end
