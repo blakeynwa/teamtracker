f44e3be998dd51333f8e5f2ee0a23eb79ec180f7
Blake Meyer <blake.ynwa@gmail.com>
2017-08-31 11:32:31 -0700

    Updated profile feature, and rendering profile page after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource_or_scope)
-    teams_path
+    # teams_path
+    user_root_url
   end
 
   def after_sign_out_path_for(resource_or_scope)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'users/profile'
+
   get 'teams/index'
 
-#resolve these 
+#resolve these
   devise_for :users
+  as :user do
+    get 'users/profile', :to => 'users#profile', :as => :user_root
+  end
+
   resources :teams
 end
