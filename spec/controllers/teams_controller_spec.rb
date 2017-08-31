3882bc9b76d8d276a5fc78c4e60cb96795970c5d
Blake Meyer <blake.ynwa@gmail.com>
2017-08-31 10:32:42 -0700

    Complete rspec buildout for teams controller and model. All tests passing

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe TeamsController, type: :controller do
+  let!(:team) { Team.create!(name: "Seagulls", sport: "Basketball", number_required_to_play: "5") }
 
   describe "GET #index" do
-    it "returns http success" do
+    it "responds with status code 200" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(200)
+    end
+
+    it "assigns teams as @teams" do 
+      get :index
+      expect(assigns(:teams)).to eq([team])
+    end
+
+    it "renders the index template" do 
+      get :index
+      expect(response).to render_template(:index)
     end
   end
 
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  team = Team.new(sport: "softball", number_required_to_play: 5)
+  team = Team.new(name: "Seagulls", sport: "softball", number_required_to_play: 5)
   describe "team is instantiated with attributes" do 
+    it "has a name" do 
+      expect(team.name).to eq("Seagulls")
+    end
+
     it "has a specific sport" do 
       expect(team.sport).to eq("softball")
     end
